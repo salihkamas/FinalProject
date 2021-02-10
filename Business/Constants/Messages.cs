b0a0575834e5c12af1349d380e6abcdb84811a85
Salih Kamaş <salihkamasy@gmail.com>
2021-02-11 00:00:17 +0300

    10.02

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,5 +9,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Product Added";
         public static string ProductNameInvalid = "Product name is invalid";
+        public static string MaintenanceTime = "System is in Maintenance";
+        public static string ProductsListed = "Products listed";
     }
 }
