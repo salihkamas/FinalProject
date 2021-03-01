58154a8cea0428dd0f9df7a2900e48f5360f3841
Salih Kamaş <salihkamasy@gmail.com>
2021-03-01 17:39:27 +0300

    27.02

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,12 @@ namespace Business.Constants
         public  static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var.";
         public  static string CategoryLimitExceded="Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
+        public static string  AuthorizationDenied="Yetkiniz Yok.";
+        public static string UserRegistered;
+        public static string UserNotFound;
+        public static string PasswordError;
+        public static string SuccessfulLogin;
+        public static string UserAlreadyExists;
+        public static string AccessTokenCreated;
     }
 }
